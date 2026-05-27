65b38c960bf3500e54572b5986ee29ea5342711b
Lysuun <62596057+Lysuun@users.noreply.github.com>
2026-05-27 21:13:51 +0200

    Add files via upload

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+const serverless = require('serverless-http'); // Importante per far girare Express come funzione su Netlify
 
 const authRoutes = require('./routes/api/v1/auth/auth');
 const shootingRoutes = require('./routes/api/v1/shooting/shooting');
@@ -8,22 +9,42 @@ const employeeRoutes = require('./routes/api/v1/employee/employee');
 
 const app = express();
 
-
 app.use(express.json());
+
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://shootingfronten.vercel.app'
+];
+
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Bloccato da policy CORS: Dominio frontend non autorizzato.'));
+    }
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token'],
   credentials: true 
 }));
 
+app.get('/api/v1/ping', (req, res) => {
+  return res.status(200).send("pong");
+});
 
 app.use('/api/v1/auth', authRoutes);
-
 app.use('/api/v1/shooting', shootingRoutes);
-
 app.use('/api/v1/employee', employeeRoutes);
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server is running on port ${process.env.PORT || 3000}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
+module.exports.handler = serverless(app);
